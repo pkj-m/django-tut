e941fbb544ebb1f2eb1c4e604f6cbe41062c91fa
Pankaj Mishra <pankaj.mishra@codenation.co.in>
2021-01-07 13:42:22 +0530

    added bootstrap

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,7 +1,26 @@
 from django.shortcuts import render
 
+posts = [
+    {
+        'author': 'Pankaj',
+        'title': 'Blog Post 1',
+        'content': 'Content of post 1',
+        'date_created': 'January 7, 2021'
+    },
+    {
+        'author': 'harsh',
+        'title': 'Blog Post 2',
+        'content': 'Content of post 2',
+        'date_created': 'January 3, 2021'
+    },
+
+]
+
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return render(request, 'blog/about.html')
+    return render(request, 'blog/about.html', {'title': 'Pankaj'})
